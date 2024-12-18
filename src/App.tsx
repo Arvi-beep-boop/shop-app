f80bd9c8dfc3bb2cf17a967e8b0c41b31d56b5a8
unknown <xarvichanx@gmail.com>
2024-12-18 19:15:07 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Button } from "antd";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Button type="default">Button</Button>
+    </div>
+  );
+}
